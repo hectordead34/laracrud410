35fe034d9afd223ffd20aacddf0f8a287087f6aa
hectordead34 <23610169@utgz.edu.mx>
2024-10-11 13:58:33 -0600

    Laracrud punto inicial

--- routes/web.php
+++ routes/web.php
@@ -19,35 +19,3 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/testView', function () {
-return view('testView');
-});
-
-Route::get('/texto', function () {
-    echo "Hi world";
-    echo "<h1>Hola mundo</h1>";
-    });
-    
-    Route::get('/echo', function () {
-        echo view('text2');
-        });
-
-        Route::get('/MasRutas', function () {
-            return "podemo tener mas rutas tipo get, POST
-            DELETE , Path";
-
-            });
-
-            Route::get('/custom', function () {
-               $msj=" Mensaje desde el servidor ";
-               $id=" 500 ";
-      return view('custom',['msj'=>$msj,"Identy"=>$id,"A"=> 29 ]);
-            });
-           
-            Route::get('/contact', function () {
-                return view('contacto');
-            })-> name('contact');
-            
-            Route::get('/testdb',[TestController::class,'test']);
-
-
