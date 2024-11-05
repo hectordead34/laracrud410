aabfed884da05f0094f54c8ca574ae4fb7174392
hectordead34 <23610169@utgz.edu.mx>
2024-11-05 13:49:59 -0600

    Segunda parte modificada en marcas

---
+++ database/migrations/2024_10_22_201507_create_brands_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('brands', function (Blueprint $table) {
+            $table->id();
+            $table->string('brand', 40)->nullable()->default('Marca');
+            $table->string('description', 100)->nullable()->default('Descripcion');
+            
+            $table->timestamps();
+            
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('brands');
+    }
+};
--- database/migrations/2024_10_22_201508_create_products_table.php
+++ database/migrations/2024_10_22_201508_create_products_table.php
@@ -14,12 +14,15 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name_product', 50)->nullable()->default('producto');
-            $table->string('brand', 100)->nullable()->default('marca');
+            $table->foreignId('brand_id');
+           // $table->string('brand', 100)->nullable()->default('marca');
             $table->integer('stock')->unsigned()->nullable()->default(12);
             $table->decimal('unit_price', 5, 2)->nullable()->default(123.45);
             $table->string('image', 100)->nullable()->default('imagen');
 
             $table->timestamps();
+             $table->foreign('brand_id')->references('id')->on('brands')->onDelete('cascade')
+                 ->onUpdate('cascade');
         });
     }
 
